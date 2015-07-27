9439b7a3d88400ba72dbca137492f4c47a4ec8a4
Sangeeth Kumar <sangeeth.kumar@gmail.com>
2015-07-27 10:00:05 +0530

    hello-world: Minor code improvements

--- hello-world/src/main/java/com/sangeethlabs/storm/basic/Main1.java
+++ hello-world/src/main/java/com/sangeethlabs/storm/basic/Main1.java
@@ -1,6 +1,7 @@
 package com.sangeethlabs.storm.basic;
 
 import java.util.Arrays;
+import java.util.List;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
@@ -12,6 +13,9 @@ import backtype.storm.utils.Utils;
  * This example works only on local Storm cluster.
  */
 public class Main1 {
+    private static final List<String> ZOOKEEPER_SERVERS=Arrays.asList("192.168.59.103");
+    private static final String NIMBUS_HOST="192.168.59.103";
+    private static final String LOG_SERVER_HOST="192.168.59.3";
     public static void main(String[] args) throws Exception {
         StormTopology topology = createTopology();
         
@@ -22,10 +26,10 @@ public class Main1 {
 //        conf.put(XmppBolt.XMPP_SERVER, "192.168.59.103");
 //        conf.put(XmppBolt.XMPP_TO, "bose@domain.local");
         conf.put(Config.TOPOLOGY_DEBUG, true);
-        conf.put(Config.NIMBUS_HOST, "192.168.59.103");
+        conf.put(Config.NIMBUS_HOST, NIMBUS_HOST);
         conf.put(Config.NIMBUS_THRIFT_PORT, 6627);
         conf.put(Config.STORM_ZOOKEEPER_PORT, 2181);
-        conf.put(Config.STORM_ZOOKEEPER_SERVERS, Arrays.asList("192.168.59.103"));
+        conf.put(Config.STORM_ZOOKEEPER_SERVERS, ZOOKEEPER_SERVERS);
 
         LocalCluster cluster = new LocalCluster();
         cluster.submitTopology("test", conf, topology);
@@ -38,8 +42,8 @@ public class Main1 {
         TopologyBuilder builder = new TopologyBuilder();
 
         builder.setSpout("spout", 
-                          new WordsKafkaSpout(/*logServerHost=*/"192.168.59.3",
-                                               /*kafkaHost=*/"192.168.59.103", 
+                          new WordsKafkaSpout(/*logServerHost=*/LOG_SERVER_HOST,
+                                               /*kafkaHost=*/ZOOKEEPER_SERVERS.get(0), 
                                                /*topic=*/"words-stream", 
                                                /*id=*/"helloworld-main1") , 
                         1);
@@ -48,7 +52,7 @@ public class Main1 {
         builder.setBolt("filter", new WordFilterBolt(/*The word to be filter from processing is */ "using"), 1).shuffleGrouping("exclaim1");
         builder.setBolt("exclaim2", new ExclamationBolt(), 1).shuffleGrouping("filter");
 //        builder.setBolt("notify", new XmppBolt(), 1).shuffleGrouping("exclaim2");
-        builder.setBolt("logger", new LoggerBolt(/*Host IP*/"192.168.59.3"), 1).shuffleGrouping("exclaim2");
+        builder.setBolt("logger", new LoggerBolt(/*Host IP*/LOG_SERVER_HOST), 1).shuffleGrouping("exclaim2");
         return builder.createTopology();
     }
 }
