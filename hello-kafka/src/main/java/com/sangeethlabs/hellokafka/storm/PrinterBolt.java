92fed34d826931094563f38821181537a6018b20
Sangeeth <sangeeth.kumar@gmail.com>
2015-12-16 12:21:49 +0530

    hello-kafka:Using logger instead of stdout.Deserializing obj from bytes.

--- hello-kafka/src/main/java/com/sangeethlabs/hellokafka/storm/PrinterBolt.java
+++ hello-kafka/src/main/java/com/sangeethlabs/hellokafka/storm/PrinterBolt.java
@@ -1,5 +1,11 @@
 package com.sangeethlabs.hellokafka.storm;
 
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
@@ -7,13 +13,29 @@ import backtype.storm.tuple.Tuple;
 
 public class PrinterBolt extends BaseBasicBolt {
     private static final long serialVersionUID = 1L;
+    
+    private static final Logger logger = LoggerFactory.getLogger(PrinterBolt.class);
 
     @Override
     public void execute(Tuple tuple, BasicOutputCollector collector) {
-        System.out.printf("PRINTER: %s\n", tuple.getValueByField("message"));
+        byte [] bytes = (byte[])tuple.getValueByField("message");
+        try {
+            logger.info("Value: {}", toObject(bytes));
+        } catch (Exception e) {
+            logger.error("Failed to parse the message", e);
+        }
     }
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer ofd) {
     }
+    
+    private static Object toObject(byte[] bytes) throws Exception {
+        Object object = null;
+        ByteArrayInputStream bin = new ByteArrayInputStream(bytes);
+        ObjectInputStream in = new ObjectInputStream(bin);
+        object = in.readObject();
+        in.close();
+        return object;
+    }    
 }
